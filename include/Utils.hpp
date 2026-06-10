c7d90fa56f648810f830061cb75c19a07983aab7
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-06-10 22:29:15 +0530

    Add RNG Class and some formatting changes

--- include/Utils.hpp
+++ include/Utils.hpp
@@ -2,37 +2,37 @@
 
 #include <optional>
 #include <random>
-#include <vector>
 
-class Rng
-{
+class Rng {
 private:
-    std::random_device rd_;
-    std::mt19937 generator_;
-    std::uniform_real_distribution<double> distribution_;
-    double min_;
-    double max_;
+  std::random_device rd_;
+  std::mt19937 generator_;
+  std::uniform_real_distribution<double> distribution_;
+  double min_;
+  double max_;
 
 public:
-    Rng(std::optional<long> seed, double min = 0.0, double max = 1.0)
-        : min_(min),
-          max_(max),
-          generator_(seed.has_value() ? std::mt19937(*seed) : std::mt19937(rd_())),
-          distribution_(min_, max_)
-    {
-    }
+  Rng(std::optional<long> seed, double min = 0.0, double max = 1.0)
+      : min_(min), max_(max),
+        generator_(seed.has_value() ? std::mt19937(*seed)
+                                    : std::mt19937(rd_())),
+        distribution_(min_, max_) {}
 
-    double next()
-    {
-        return distribution_(generator_);
-    }
+  double next() { return distribution_(generator_); }
 
-    template <typename T>
-    T choose(const T &a, const T &b)
-    {
-        if (next() < 0.5)
-            return a;
-        else
-            return b;
-    }
+  template <typename T> T choose(const T &a, const T &b) {
+    if (next() < 0.5)
+      return a;
+    else
+      return b;
+  }
+};
+
+class GenomeIndexer {
+private:
+  int next_id_;
+
+public:
+  GenomeIndexer(int next_id = 0) : next_id_(next_id) {};
+  int next() { return next_id_++; }
 };
