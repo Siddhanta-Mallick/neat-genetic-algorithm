31ff2fc34a7a8a7662c47dcf32589ce8804fef1e
Siddhanta Mallick <sid.mallick1947@gmail.com>
2026-06-11 20:23:47 +0530

    Add header for Mutation

---
+++ include/Mutation.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Genotype.hpp"
+
+class Mutation {
+private:
+  double clamp(double x) const;
+
+public:
+  void mutate_add_link(Genome &genome);
+  void mutate_remove_link(Genome &genome);
+  void mutate_add_neuron(Genome &genome);
+  void mutate_remove_neuron(Genome &genome);
+  double mutate_new_value();
+  double mutate_delta();
+};
